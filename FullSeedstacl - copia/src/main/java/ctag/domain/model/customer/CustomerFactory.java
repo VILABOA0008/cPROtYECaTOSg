1990e7d4a27cc878e267e6c639bda32ce26c0dc8
Pablo Vilaboa Costas <pabcos@ctag>
2019-05-06 12:58:05 +0200

    FIN

--- FullSeedstacl - copia/src/main/java/ctag/domain/model/customer/CustomerFactory.java
+++
@@ -1,9 +0,0 @@
-package ctag.domain.model.customer;
-
-import org.seedstack.business.domain.Factory;
-
-public interface CustomerFactory extends Factory<Customer>{
-  
-  Customer createCustomer(String name, int age);
-
-}
